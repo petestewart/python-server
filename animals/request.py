64f22a6da5b2fa999465c7d71d30899e17823c74
Pete Stewart <petestew@gmail.com>
2020-10-14 11:58:54 -0500

    delete animals request works

--- animals/request.py
+++ animals/request.py
@@ -56,19 +56,13 @@ def update_animal(id, new_animal):
             break
 
 def delete_animal(id):
-    # Initial -1 value for animal index, in case one isn't found
-    animal_index = -1
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
 
-    # Iterate the ANIMALS list, but use enumerate() so that we
-    # can access the index value of each item
-    for index, animal in enumerate(ANIMALS):
-        if animal["id"] == id:
-            # Found the animal. Store the index.
-            animal_index = index
-
-    # If the animal was found, use pop(int) to remove it from list
-    if animal_index >= 0:
-        ANIMALS.pop(animal_index)
+        db_cursor.execute("""
+        DELETE FROM Animal
+        WHERE id = ?
+        """, (id, ))
 
 def get_all_animals():
     # Open a connection to the database
